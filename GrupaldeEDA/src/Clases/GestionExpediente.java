24d9f31e9714a7586400f07ca333bef3111224e1
fespa <fespa@GBFE>
2025-07-05 16:53:52 -0500

    Creación de Nodo y Cola para expedientes

--- GrupaldeEDA/src/Clases/GestionExpediente.java
+++ GrupaldeEDA/src/Clases/GestionExpediente.java
@@ -1,5 +1,66 @@
 package Clases;
-public class GestionExpediente {
+public class GestionExpediente<T> {
+    
+    private Nodo<T> frente;
+    private Nodo<T> ultimo;
+    
+    public GestionExpediente() {
+        frente = null;
+        ultimo = null;
+    }
+    
+    public boolean esVacia() {
+        if (frente == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public void encolar(T pElemento){
+        if (esVacia() == true){
+            Nodo<T> nuevoNodo = new Nodo<T>(pElemento, null);
+            frente = nuevoNodo;
+            ultimo = nuevoNodo;
+        } else { // La cola no está vacía
+            Nodo<T> nuevoNodo = new Nodo<T>(pElemento, null);
+            ultimo.setSgteNodo(nuevoNodo);
+            ultimo = nuevoNodo;
+        }
+    }
+    
+    public T desencolar() {
+        if (esVacia()) {
+            return null; // o puedes lanzar una excepción si prefieres
+        } else {
+            T elemento = frente.getElemento();
+            frente = frente.getSgteNodo();
+            if (frente == null) {
+                ultimo = null; // si la cola queda vacía
+            }
+            return elemento;
+        }
+    }
+    
+    public void MostrarCola() {
+        Nodo<T> aux = frente;
+        while (aux != null) {
+            System.out.print(" " + aux.getElemento());
+            aux = aux.getSgteNodo();
+        }
+    }
+    
+    public void InsertarFrente(T Elemento) {
+        if (esVacia() == true) {
+            Nodo<T> nuevoNodo = new Nodo<T>(Elemento, null);
+            frente = nuevoNodo;
+            ultimo = nuevoNodo;
+        } else { // La cola no está vacía
+            Nodo<T> nuevoNodo = new Nodo<T>(Elemento, null);
+            nuevoNodo.setSgteNodo(frente);
+            frente = nuevoNodo;
+        }
+    }
     
     
     
