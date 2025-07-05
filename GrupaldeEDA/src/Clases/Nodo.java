d55f4d82a5370abc5d99ce5c18fe911082915b52
fespa <fespa@GBFE>
2025-07-05 17:55:08 -0500

    Agregado funciones a la clase GestionExpediente (punto 1 y 2 terminados), ademas de crear los clases cuentaAdministrador y Demo(temporal para verificar funcionalidad)

--- GrupaldeEDA/src/Clases/Nodo.java
+++ GrupaldeEDA/src/Clases/Nodo.java
@@ -7,25 +7,25 @@ public class Nodo<T> {
         elemento = null;
         sgteNodo = null;
     }
-    
-    public Nodo(T pElemento, Nodo pSgteNodo){
-        elemento = pElemento;
-        sgteNodo = pSgteNodo;
-    }
-    
-    public void setElemento(T pElemento){
-        elemento = pElemento;
+
+    public Nodo(T elemento, Nodo sgteNodo) {
+        this.elemento = elemento;
+        this.sgteNodo = sgteNodo;
     }
 
-    public T getElemento(){
+    public T getElemento() {
         return elemento;
     }
 
-    public void setSgteNodo(Nodo pSgteNodo){
-        sgteNodo = pSgteNodo;
+    public void setElemento(T elemento) {
+        this.elemento = elemento;
     }
 
-    public Nodo getSgteNodo(){
+    public Nodo getSgteNodo() {
         return sgteNodo;
+    }
+
+    public void setSgteNodo(Nodo sgteNodo) {
+        this.sgteNodo = sgteNodo;
     }
 }
