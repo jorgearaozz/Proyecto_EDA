635af51e458a69650bd6ea725f9ee01a80d110e9
PC <PC@DESKTOP-N30KG2B>
2025-07-05 19:31:49 -0500

    solucion de errores a la hora de correrlo

--- GrupaldeEDA/src/Clases/Demo_EliminarLuego_.java
+++ GrupaldeEDA/src/Clases/Demo_EliminarLuego_.java
@@ -1,10 +1,15 @@
 package Clases;
+
+import View.Menu;
+
 public class Demo_EliminarLuego_ {
     public static void main(String[] args) {
-        Expediente Expediente1 = new Expediente("","","",0,"","","","");
+        Expediente Expediente1 = new Expediente("","","",0,"","","","","");
         GestionExpediente Tramite1 = new GestionExpediente();
         
         Tramite1.InsertarFrente(Expediente1);
         Tramite1.MostrarCola();
+        Menu x = new Menu();
+        x.setVisible(true);
     }
 }
