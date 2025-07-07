fe6b2b0fc9c021d66c5ad6e492cad4f40a42f213
fespa <fespa@GBFE>
2025-07-06 19:09:54 -0500

    Agregado la clase Local con sus funciones principales

--- GrupaldeEDA/src/Clases/Demo_EliminarLuego_.java
+++ GrupaldeEDA/src/Clases/Demo_EliminarLuego_.java
@@ -1,18 +1,29 @@
 package Clases;
-
 import View.Menu;
 
 public class Demo_EliminarLuego_ {
     public static void main(String[] args) {
-        Expediente Expediente1 = new Expediente("","","",0,"","","","","");
-        GestionExpediente Tramite1 = new GestionExpediente();
+        Local Local1 = new Local();
+        //Expediente(id,prioridad(0|1),asunto,documento,dni,nombre,telefono,correo)
+        Expediente Expediente1 = new Expediente(1, 0, "asunto1", "documento1", 1, "Fabricio", "123", "@1");
+        Expediente Expediente2 = new Expediente(2, 1, "asunto2", "documento2", 2, "Jorge", "456", "@2");
+        Expediente Expediente3 = new Expediente(2, 1, "asunto3", "documento3", 2, "Gustavo", "789", "@3");
         
-        Tramite1.InsertarFrente(Expediente1);
-        Tramite1.MostrarCola();
-        Tramite1.agregarALista(Expediente1);
-        Tramite1.mostrarLista();
+        //Local Funcs: 
+        //Ingresar(Ingresa expediente),
+        //MostrarColas(Muestra colas regulares y prioridad),
+        //FinTramite(desencola y muestra datos)
+        Local1.Ingresar(Expediente1);
+        Local1.Ingresar(Expediente2);
+        Local1.Ingresar(Expediente3);
+        Local1.MostrarColas();
+        System.out.println("\n--\n");
+        System.out.println(Local1.FinTramite(Expediente3));
         
-        Menu x = new Menu();
-        x.setVisible(true);
+        
+        /*
+          Menu x = new Menu();
+          x.setVisible(true);
+         */
     }
 }
